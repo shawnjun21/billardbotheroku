aa643508c4e5350130683c4a616289ea8a399c91
shawnjun21 <35420577+shawnjun21@users.noreply.github.com>
2018-01-31 13:22:19 -1000

    Update billardbot.js

--- billardbot.js
+++ billardbot.js
@@ -4,7 +4,6 @@
 
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const token = "ToKeN";
 
 const OPINION_LIKE = 1;
 const OPINION_NEUTRAL = 2;
@@ -675,4 +674,4 @@ bot.on("message", message =>
 	}
 });
 
-bot.login(token);
+bot.login(process.env.BOT_TOKEN);
